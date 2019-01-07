834c951f7e46e78403d38c67b15cbf080f6eec83
Russel Simmons <russ@rsimmons.org>
2019-01-07 13:30:01 -0800

    Clean up first sketch a bit

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,7 +1,7 @@
 const {makeSketch} = require('./util');
 
 makeSketch({x: 64, y: 64, z: 64}, {padFrac: 0.1}, (scene, ops) => {
-  // ops.fillBox(scene, vec3Create(0, 0, 0), vec3Create(1000, 1000, 1000), 1);
+  // ops.fillScene(scene); // uncomment for nice variation
   for (let i = 0; i < 1000; i++) {
     ops.randomBox(scene, (Math.random() < 0.85) ? 0 : 1);
   }
