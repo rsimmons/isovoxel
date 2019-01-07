b59d5a504783881128d3bfa926b9d826014408d4
Russel Simmons <russ@rsimmons.org>
2019-01-07 14:28:49 -0800

    Add op for hollow shell, sketch using it

--- sketches/sketch001.js
+++ sketches/sketch001.js
@@ -1,9 +1,8 @@
 const {makeSketch} = require('./util');
 
 makeSketch({x: 64, y: 64, z: 64}, {padFrac: 0.1}, (scene, ops) => {
-  ops.fillScene(scene);
-  for (let i = 0; i < 1000; i++) {
-    ops.randomBox(scene, (Math.random() < 0.5) ? 0 : 1);
+  for (let i = 0; i < 100; i++) {
+    ops.randomShell(scene, 1);
   }
   ops.frontCutaway(scene, 0.7);
 });
