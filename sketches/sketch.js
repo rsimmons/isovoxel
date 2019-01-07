533bd7a1bf9813598c849248bfae0d06c1401d14
Russel Simmons <russ@rsimmons.org>
2019-01-07 11:46:56 -0800

    Split code out into logical files

---
+++ sketches/sketch.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const {createScene, fillBox, randVoxelVec} = require('../lib/scene');
+const {renderSceneToSVG} = require('../lib/render');
+
+const scene = createScene(64, 64, 64);
+
+// fillBox(scene, vec3Create(0, 0, 0), vec3Create(1000, 1000, 1000), 1);
+for (let i = 0; i < 1000; i++) {
+  fillBox(scene, randVoxelVec(scene), randVoxelVec(scene), (Math.random() < 0.85) ? 0 : 1);
+}
+
+const svg = renderSceneToSVG(scene, 0.1, true);
+fs.writeFileSync('out.svg', svg);
