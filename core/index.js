bc5eff4a43fff319729c157ef38e3dc7077c053b
Russel Simmons <russ@rsimmons.org>
2019-01-10 13:11:45 -0800

    Make yarn workspaces, add ignores

---
+++ core/index.js
@@ -0,0 +1,8 @@
+const {createScene, ops} = require('./scene');
+const {renderScene} = require('./render');
+
+module.exports = {
+  createScene,
+  ops,
+  renderScene,
+};
--- sketches/util.js
+++ sketches/util.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
-const {createScene, ops} = require('../lib/scene');
-const {renderScene} = require('../lib/render');
+const {createScene, ops, renderScene} = require('isovoxel-core');
 
 function makeSketch(size, svgOpts, genFunc) {
   const scene = createScene(size);
