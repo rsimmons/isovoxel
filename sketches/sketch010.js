4d2e1ca383603c790f02715b8086e37705df271e
Russel Simmons <russ@rsimmons.org>
2019-01-15 16:41:57 -0800

    Add sketch

---
+++ sketches/sketch010.js
@@ -0,0 +1,12 @@
+const {makeSketch} = require('./util');
+
+// This is meant to be approximately square and have thicker lines for laser cutting
+
+makeSketch({x: 32, y: 32, z: 40}, {padFrac: 0.1}, (scene, ops) => {
+  for (let i = 0; i < 200; i++) {
+    ops.randomShell(scene, 1);
+  }
+  ops.predicateFill(scene, 0, (x, y, z) => ((x % 8) < 2));
+  ops.predicateFill(scene, 0, (x, y, z) => ((z % 16) < 4));
+  ops.frontCutaway(scene, 0.5);
+});
