74868edf334d4ecf74bbabab3c759fcbb2565ccb
Russel Simmons <russ@rsimmons.org>
2019-01-17 20:43:18 -0800

    Clean up sketch

--- sketches/sketch020.js
+++ sketches/sketch020.js
@@ -1,14 +1,6 @@
 const {makeSketch} = require('./util');
 
 makeSketch({x: 128, y: 128, z: 128}, {padFrac: 0.1}, (scene, ops) => {
-/*
-  for (let i = 0; i < 256; i++) {
-    ops.randomShell(scene, 1);
-  }
-  const IN = 32;
-  ops.predicateFill(scene, 0, (x, y, z) => ((x < IN) || (y < IN) || (x >= (scene.size.x-IN)) || (y >= (scene.size.y-IN)) || (z >= 32)));
-*/
-
   ops.makeHollowShell(scene, {x: 0, y: 0, z: 0}, {x: scene.size.x, y: scene.size.y, z: 8}, 1);
   ops.predicateFill(scene, 0, (x, y, z) => ((y % 16) < 6));
 
@@ -18,9 +10,6 @@ makeSketch({x: 128, y: 128, z: 128}, {padFrac: 0.1}, (scene, ops) => {
 
   const RAD = 6;
   ops.makeHollowShell(scene, {x: 64-RAD, y: 64-RAD, z: 0}, {x: 64+RAD, y: 64+RAD, z: 128}, 1);
-
-  // ops.predicateFill(scene, 0, (x, y, z) => ((x % 16) < 4));
-  // ops.predicateFill(scene, 0, (x, y, z) => ((z % 16) < 4));
 
   ops.frontCutaway(scene, 0.5);
 });
